92ab99dc2898f67e1f39619d51617701e287a6a6
Michel Fortes <michelfortes@2waytech.com.br>
2025-03-23 23:23:48 -0300

    first commit

---
+++ build/plugin/go.mod
@@ -0,0 +1,3 @@
+module deprecated-header-plugin
+
+go 1.22.5
---
+++ build/plugin/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+)
+
+type ResponseWrapper interface {
+	Data() map[string]interface{}
+	Io() io.Reader
+	IsComplete() bool
+	StatusCode() int
+	Headers() map[string][]string
+}
+
+var errUnknownType = errors.New("unknown request type")
+
+func main() {}
+
+func init() {
+	fmt.Println(string(ModifierRegisterer), "loaded!")
+}
+
+var ModifierRegisterer = registerer("deprecated-header")
+
+type registerer string
+
+func (r registerer) RegisterModifiers(f func(
+	name string,
+	factoryFunc func(map[string]interface{}) func(interface{}) (interface{}, error),
+	appliesToRequest bool,
+	appliesToResponse bool,
+)) {
+	f(string(r), r.responseHandler, false, true)
+}
+
+func (r registerer) responseHandler(
+	cfg map[string]interface{},
+) func(interface{}) (interface{}, error) {
+
+	pluginConfig, ok := cfg["deprecated-header"]
+	if !ok {
+		return func(input interface{}) (interface{}, error) {
+			return input, errors.New("missing deprecated-header config")
+		}
+	}
+
+	config, ok := pluginConfig.(map[string]interface{})
+	if !ok {
+		return func(input interface{}) (interface{}, error) {
+			return input, errors.New("invalid deprecated-header config")
+		}
+	}
+
+	cfgDeprecated, ok := config["deprecated"].(string)
+	if !ok {
+		return func(input interface{}) (interface{}, error) {
+			return input, errors.New("missing deprecated-header 'deprecated' config")
+		}
+	}
+
+	cfgSunset, ok := config["sunset"].(string)
+	if !ok {
+		return func(input interface{}) (interface{}, error) {
+			return input, errors.New("missing deprecated-header 'sunset' config")
+		}
+	}
+
+	cfgLink, ok := config["link"].(string)
+	if !ok {
+		return func(input interface{}) (interface{}, error) {
+			return input, errors.New("missing deprecated-header 'link' config")
+		}
+	}
+
+	return func(input interface{}) (interface{}, error) {
+
+		resp, ok := input.(ResponseWrapper)
+		if !ok {
+			return nil, errUnknownType
+		}
+
+		if resp.Headers() != nil {
+
+			resp.Headers()["Deprecated"] = []string{cfgDeprecated}
+			resp.Headers()["Sunset"] = []string{cfgSunset}
+			resp.Headers()["Link"] = []string{cfgLink}
+		}
+
+		return input, nil
+	}
+}
